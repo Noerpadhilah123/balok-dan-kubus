795438e1e4ed4090e0f548fc59da73181d52a799
Noerpadhilah123 <113620279+Noerpadhilah123@users.noreply.github.com>
2022-09-15 23:08:58 +0700

    Add files via upload

---
+++ balok dan kubus.py
@@ -0,0 +1,14 @@
+# MENGHITUNG KUBUS
+
+print("menghitung volume kubus")
+sisi= float(input("masukkan sisi: "))
+hasil =sisi+sisi+sisi
+print("volume kubus adalah : " + str (hasil))
+
+
+# MENGHITUNG KUBUS
+
+print("menghitung volume kubus")
+sisi= float(input("masukkan sisi: "))
+hasil =sisi+sisi+sisi
+print("volume kubus adalah : " + str (hasil))
